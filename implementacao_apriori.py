74d3f21a5382a089e6136c78683dfe79ff8eae84
Albert <albertmdepaula@gmail.com>
2020-06-21 17:01:39 -0300

    c1 - inserindo o codigo e os dados da pizzaria

---
+++ implementacao_apriori.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 20 17:58:00 2020
+
+@author: Albert
+"""
+
+import pandas as pd
+
+dados = pd.read_csv(r'C:\Users\Albert\Downloads\pizzaria.csv', header = None)
+transacoes = []
+for i in range(0,2244):
+    transacoes.append([str(dados.values[i,j]) for j in range (0, 7)])
+
+from apyori import apriori
+regras = apriori(transacoes, min_support = 0.3, min_confidence = 0.8, min_lift = 1)
+
+resultados = list(regras)
+resultados
+
+resultados2 = [list(x) for x in resultados]
+resultados2
+
+resultadoFormatado = []
+for j in range(0, 7):
+    resultadoFormatado.append([list(x) for x in resultados2[j][2]])
+
+resultadoFormatado
